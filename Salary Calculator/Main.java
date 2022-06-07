8c79af1618ce335604cb12cc159201bc27798cf1
Firat Kara <60990126+firatkara1@users.noreply.github.com>
2022-06-07 17:37:14 +0300

    Project files uploaded

---
+++ Salary Calculator/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Firat", 2000, 45, 2022);
+        Employee e2 = new Employee("Fatma", 2200, 50, 2015);
+
+        e1.toStrings();
+        System.out.println("----------------------------------------");
+        e2.toStrings();
+
+    }
+
+}
