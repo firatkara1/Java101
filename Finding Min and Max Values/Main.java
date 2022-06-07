9d733f1bc16cfffcb0454ce6abf5b72ebad36cc9
Firat Kara <60990126+firatkara1@users.noreply.github.com>
2022-06-07 17:51:30 +0300

    Update Main.java

--- Finding Min and Max Values/Main.java
+++ Finding Min and Max Values/Main.java
@@ -1,1 +1,33 @@
 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int total,input,min=0,max=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kac sayi gireceksiniz: ");
+        total = sc.nextInt();
+
+        for(int i = 1; i <= total; i++){
+            System.out.print(i + ". Sayiyi giriniz: "  );
+            input = sc.nextInt();
+
+            if (i==1){
+                max = input;
+                min = input;
+            }else{
+                if (input > max){
+                    max = input;
+                } else if (input < min) {
+                    min = input;
+                }
+            }
+        }
+        System.out.println("En buyuk sayi = " + max);
+        System.out.println("En kucuk sayi = " + min);
+
+    }
+
+}
